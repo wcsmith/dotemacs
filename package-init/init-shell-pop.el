faab748908333e89a70ed26e00a10b68ab967f4b
wcsmith <wconnorsmith@gmail.com>
2014-03-14 12:58:18 -0700

    Immediately load shell-pop.

--- package-init/init-shell-pop.el
+++ package-init/init-shell-pop.el
@@ -1,10 +1,11 @@
-(after 'shell-pop
-  (setq shell-pop-internal-mode "ansi-term")
-  (setq shell-pop-internal-mode-func
-	(lambda () (ansi-term (getenv "SHELL"))))
-  (setq shell-pop-internal-mode-buffer "*ansi-term*")
-  (setq shell-pop-window-height 40)
-  (setq shell-pop-autocd-to-working-dir nil))
+(require 'shell-pop)
+
+(setq shell-pop-internal-mode "ansi-term")
+(setq shell-pop-internal-mode-func
+      (lambda () (ansi-term (getenv "SHELL"))))
+(setq shell-pop-internal-mode-buffer "*ansi-term*")
+(setq shell-pop-window-height 40)
+(setq shell-pop-autocd-to-working-dir nil)
 
 ;; ipython
 (defun shell-pop-special (shell-cmd)
