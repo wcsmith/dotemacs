33ea81f213bfc26a58ceedb2cc973609fe37bb2d
wcsmith <wconnorsmith@gmail.com>
2014-03-26 13:35:51 -0700

    Put ido keybinding assignments into setup hook.

--- package-init/init-ido.el
+++ package-init/init-ido.el
@@ -1,7 +1,14 @@
 (require 'ido)
 
 (defun my-ido-setup ()
-  (define-key ido-file-completion-map "+" 'ido-make-directory))
+  (define-key ido-file-completion-map "+" 'ido-make-directory)
+  (define-key ido-file-completion-map "\M-k" 'ido-prev-match)
+  (define-key ido-common-completion-map "\M-j" 'ido-next-match)
+  (define-key ido-common-completion-map "\M-k" 'ido-prev-match)
+  (define-key ido-common-completion-map (kbd "<M-return>") 'ido-select-text)
+  (define-key minibuffer-local-map "\M-j" 'ido-next-match)
+  (define-key minibuffer-local-map "\M-k" 'ido-prev-match)
+  (define-key minibuffer-local-map (kbd "<M-return>") 'ido-select-text))
 (add-hook 'ido-setup-hook 'my-ido-setup)
 
 (setq ido-enable-flex-matching t)
@@ -10,11 +17,6 @@
 (setq ido-auto-merge-delay-time nil)
 (setq ido-auto-merge-work-directories-length -1)
 
-(define-key ido-common-completion-map "\M-j" 'ido-next-match)
-(define-key ido-common-completion-map "\M-k" 'ido-prev-match)
-(define-key ido-common-completion-map (kbd "<M-return>") 'ido-select-text)
-(define-key minibuffer-local-map "\M-j" 'ido-next-match)
-(define-key minibuffer-local-map "\M-k" 'ido-prev-match)
-(define-key minibuffer-local-map (kbd "<M-return>") 'ido-select-text)
+
 
 (ido-mode t)
