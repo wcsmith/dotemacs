04385e35dcff47c900c61c99bf2e7c91dfe20a04
wcsmith <wconnorsmith@gmail.com>
2014-03-25 22:02:57 -0700

    Flycheck on save or mode-enabled only.

--- package-init/init-flycheck.el
+++ package-init/init-flycheck.el
@@ -1,5 +1,7 @@
 (require 'flycheck)
 
+(setq flycheck-check-syntax-automatically '(save mode-enabled))
+
 (add-hook 'c-mode-common-hook '(lambda () (flycheck-mode)))
 (add-hook 'python-mode-hook '(lambda () (flycheck-mode)))
 (add-hook 'cperl-mode-hook '(lambda () (flycheck-mode)))
