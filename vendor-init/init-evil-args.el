952b9128aefdf25e78fcd172ef79894a7350880b
wcsmith <wconnorsmith@gmail.com>
2014-03-26 13:37:18 -0700

    Added evil-args commands to motion state.

--- vendor-init/init-evil-args.el
+++ vendor-init/init-evil-args.el
@@ -2,6 +2,8 @@
 
 (define-key evil-normal-state-map "L" 'evil-forward-arg)
 (define-key evil-normal-state-map "H" 'evil-backward-arg)
+(define-key evil-motion-state-map "L" 'evil-forward-arg)
+(define-key evil-motion-state-map "H" 'evil-backward-arg)
 
 (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
 (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
