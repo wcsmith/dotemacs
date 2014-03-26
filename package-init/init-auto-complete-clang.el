15b8aa7948fc042956b4cbfaad5e335f83dcd270
wcsmith <wconnorsmith@gmail.com>
2014-03-25 22:00:20 -0700

    Add C++ includes to clang.

--- package-init/init-auto-complete-clang.el
+++ package-init/init-auto-complete-clang.el
@@ -1,5 +1,20 @@
 (require 'auto-complete-clang)
 
+(setq clang-completion-supress-error 't)
+
+(setq ac-clang-flags
+      (mapcar (lambda (item)(concat "-I" item))
+              (split-string
+               "
+/usr/lib/gcc/x86_64-redhat-linux/4.8.2/../../../../include/c++/4.8.2
+/usr/lib/gcc/x86_64-redhat-linux/4.8.2/../../../../include/c++/4.8.2/x86_64-redhat-linux
+/usr/lib/gcc/x86_64-redhat-linux/4.8.2/../../../../include/c++/4.8.2/backward
+/usr/lib/gcc/x86_64-redhat-linux/4.8.2/include
+/usr/local/include
+/usr/include
+"
+               )))
+
 (defun setup-ac-clang ()
   (setq ac-sources '(ac-source-clang))
   (setq-local ac-auto-start t)
