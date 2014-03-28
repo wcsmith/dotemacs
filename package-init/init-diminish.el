e1b8883182be8477e26805f31473812dbdb31ade
wcsmith <wconnorsmith@gmail.com>
2014-03-28 11:40:46 -0700

    Diminish common minor modes.

--- package-init/init-diminish.el
+++ package-init/init-diminish.el
@@ -1,1 +1,15 @@
 (require 'diminish)
+
+(defmacro diminish-after (file mode to-what)
+  `(eval-after-load ,file
+     '(diminish ,mode ,to-what)))
+
+(diminish-after "projectile" 'projectile-mode nil)
+(diminish-after "autopair" 'autopair-mode nil)
+(diminish-after "auto-complete" 'auto-complete-mode "ac")
+(diminish-after "magit" 'magit-auto-revert-mode nil)
+(diminish-after "undo-tree" 'undo-tree-mode nil)
+(diminish-after "abbrev" 'abbrev-mode nil)
+(diminish-after "flycheck" 'flycheck-mode "fc")
+(diminish-after "ggtags" 'ggtags-mode "gg")
+(diminish-after "emmet-mode" 'emmet-mode "zen")
