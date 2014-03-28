2aac26dafa05ef42c74816e6ab8667ca89871071
wcsmith <wconnorsmith@gmail.com>
2014-03-28 15:21:16 -0700

    Add find-function binding to lisp mode.

--- emacs-init/init-lisp.el
+++ emacs-init/init-lisp.el
@@ -1,6 +1,7 @@
 ;; lisp mode setup
 
 (defun enable-newline-and-indent ()
-  (local-set-key (kbd "RET") 'newline-and-indent))
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  (local-set-key (kbd "M-.") 'find-function))
 (add-hook 'emacs-lisp-mode-hook 'enable-newline-and-indent)
 (add-hook 'lisp-mode-hook 'enable-newline-and-indent)
