a956189a6836860a3f7b1b2f4f9db58735845f11
wcsmith <wconnorsmith@gmail.com>
2014-03-29 17:41:23 -0700

    Keychord bindings for cc-modes.

--- package-init/init-cc-mode.el
+++ package-init/init-cc-mode.el
@@ -1,6 +1,11 @@
 (defun c++-mode-setup ()
   (after 'key-chord
     (key-chord-define c++-mode-map "--" "->")
+    (key-chord-define c-mode-base-map ";;"
+		      '(lambda () (interactive) (end-of-line) (insert ";")))
+    (key-chord-define c-mode-base-map "{{"
+		      '(lambda () (interactive) (end-of-line) (insert " {}")
+			 (backward-char)))
     (key-chord-define c-mode-map "--" "->")))
 
 (add-hook 'c++-mode-hook 'c++-mode-setup)
