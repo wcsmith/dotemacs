ee45a0946e8c87e9ecab8f60d2024155c4c8770a
wcsmith <wconnorsmith@gmail.com>
2014-03-29 17:40:26 -0700

    Pulled latest version of evil-args.

--- vendor-init/init-evil-args.el
+++
@@ -1,7 +0,0 @@
-(require 'evil-args)
-
-(define-key evil-normal-state-map "L" 'evil-forward-arg)
-(define-key evil-normal-state-map "H" 'evil-backward-arg)
-(define-key evil-motion-state-map "L" 'evil-forward-arg)
-(define-key evil-motion-state-map "H" 'evil-backward-arg)
-(define-key evil-normal-state-map "K" 'evil-jump-out-args)
