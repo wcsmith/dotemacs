7a0cdecd567d37e444cbbba57a155a873cd3da13
wcsmith <wconnorsmith@gmail.com>
2014-06-12 16:44:27 -0700

    Remove emacs-eclim

--- package-init/init-evil-leader.el
+++ package-init/init-evil-leader.el
@@ -16,9 +16,6 @@
     ;; compile (c)
     (evil-leader/set-key "c c" 'recompile)
     (evil-leader/set-key "c C" 'compile)
-    (after 'eclim
-      (evil-leader/set-key-for-mode 'java-mode "c c" 'eclim-problems-compilation-buffer)
-      (evil-leader/set-key-for-mode 'java-mode "c f" 'eclim-problems-correct))
 
     ;; gdb (d)
     (evil-leader/set-key "d g" 'gdb)
@@ -45,19 +42,4 @@
 
     ;; navigating source code (g)
     (evil-leader/set-key "g h" 'ff-find-other-file)
-    (evil-leader/set-key "g t" 'find-tag)
-
-    (after 'eclim
-      (evil-leader/set-key-for-mode 'java-mode "j d" 'eclim-java-find-references)
-      (evil-leader/set-key-for-mode 'java-mode "g d" 'eclim-java-show-documentation-for-current-element))
-
-    ;; java/eclim
-    (after 'eclim
-      (evil-leader/set-key-for-mode 'java-mode "j o" 'eclim-java-import-organize)
-      (evil-leader/set-key-for-mode 'java-mode "j i" 'eclim-java-implement)
-      (evil-leader/set-key-for-mode 'java-mode "j h" 'eclim-java-hierarchy)
-      (evil-leader/set-key-for-mode 'java-mode "j t" 'eclim-java-call-hierarchy)
-      (evil-leader/set-key-for-mode 'java-mode "j c" 'eclim-java-constructor)
-      (evil-leader/set-key-for-mode 'java-mode "j g" 'eclim-java-generate-getter-and-setter)
-      (evil-leader/set-key-for-mode 'java-mode "j r" 'eclim-java-refactor-rename-symbol-at-point)
-      (evil-leader/set-key-for-mode 'java-mode "j d" 'eclim-java-doc-comment))))
+    (evil-leader/set-key "g t" 'find-tag)))
