493b976d3ca26fc7cf10dfff5667811fdbe8c03f
wcsmith <wconnorsmith@gmail.com>
2014-06-12 17:34:47 -0700

    Customize mode line faces

--- .emacs-custom.el
+++ .emacs-custom.el
@@ -9,4 +9,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "grey65" :foreground "black"))))
+ '(mode-line-highlight ((t nil)))
+ '(mode-line-inactive ((t (:inherit mode-line :background "grey85" :foreground "grey20" :weight light)))))
