2d32e525777a8f4424494385e56d4a33b38a17ec
wcsmith <wconnorsmith@gmail.com>
2014-06-12 23:40:36 -0700

    Add modes to flycheck hook

--- package-init/init-flycheck.el
+++ package-init/init-flycheck.el
@@ -7,5 +7,8 @@
     (setq flycheck-check-syntax-automatically '(save mode-enabled))
     (hook '(c-mode-common-hook
 	    python-mode-hook
+	    ac-js2-mode
+	    ruby-mode
+	    makefile-mode
 	    cperl-mode-hook)
       (flycheck-mode))))
