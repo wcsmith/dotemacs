7651a4ed2af2e3fd00a312eb6d73b275486c7c30
wcsmith <wconnorsmith@gmail.com>
2014-07-26 10:35:27 -0700

    Remove js2 packages.

--- package-init/init-ac-js2.el
+++
@@ -1,7 +0,0 @@
-(use-package 'ac-js2
-  :install t
-  :defer t
-
-  :init
-  (hook 'js2-mode-hook
-    (ac-js2-mode)))
--- package-init/init-js2-mode.el
+++
@@ -1,6 +0,0 @@
-(use-package 'js2-mode
-  :install t
-  :defer t
-
-  :init
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
