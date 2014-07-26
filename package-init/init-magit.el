93767387dc2e6d5638ff8d7e6a0efc5f43098a4e
wcsmith <wconnorsmith@gmail.com>
2014-07-26 12:15:34 -0700

    Add search bindings to magit status mode

--- package-init/init-magit.el
+++ package-init/init-magit.el
@@ -7,6 +7,13 @@
     (define-key magit-diff-mode-map (kbd "j") 'magit-goto-next-section)
     (define-key magit-status-mode-map (kbd "k") 'magit-goto-previous-section)
     (define-key magit-status-mode-map (kbd "j") 'magit-goto-next-section)
+
+    ;; searching
+    (define-key magit-status-mode-map (kbd "/") 'evil-search-forward)
+    (define-key magit-status-mode-map (kbd "?") 'evil-search-backward)
+    (define-key magit-status-mode-map (kbd "n") 'evil-search-next)
+    (define-key magit-status-mode-map (kbd "N") 'evil-search-previous)
+
     (define-key magit-mode-map (kbd "K") 'magit-discard-item)
     (define-key magit-mode-map (kbd ":") 'evil-ex)
     (define-key magit-mode-map (kbd ";") 'magit-git-command)))
